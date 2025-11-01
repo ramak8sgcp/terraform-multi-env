d42f39395ee2bb08a80038af3c9c833a73ca1150
ramak8sgcp <ramak8sgcp@gmail.com>
2025-11-01 09:35:09 +0530

    workspace

--- workspaces/ec2.tf
+++ workspaces/ec2.tf
@@ -1,9 +1,32 @@
-resource "aws_instance" "terraform" {
-  #count = terraform.workspace == "prod" ? 2 : 1
-  ami                    = "ami-09c813fb71547fc4f"
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   instance_type          = lookup(var.instance_type, terraform.workspace)
-  vpc_security_group_ids = ["sg-0c86d154202a71583"]
   tags = {
-    Name = "terraform-${terraform.workspace}"
+    Name    = "terraform-demo-${terraform.workspace}"
+    Purpose = "terraform-practice"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls_${terraform.workspace}"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
   }
 }
